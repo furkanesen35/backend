3554bc839913f1eac9043fca6ef49ac5b4aaf7f0
Furkan Esen <furkanesen35@gmail.com>
2024-04-03 11:14:15 +0200

    zxc

--- main/settings.py
+++ main/settings.py
@@ -94,7 +94,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
  'http://localhost:3000',
- 'https://vercel.app/',
+ 'https://vercel.app',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
