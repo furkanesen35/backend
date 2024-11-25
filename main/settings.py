5483751dafcd709619cb5a6d2ad2dacdcd56ec6d
duffgor <fesn34@gmail.com>
2024-11-25 23:26:05 +0100

    back to asd

--- main/settings.py
+++ main/settings.py
@@ -104,14 +104,10 @@ CORS_ALLOW_ALL_ORIGINS = True
 REST_FRAMEWORK = {
  'DEFAULT_AUTHENTICATION_CLASSES': [
   'rest_framework_simplejwt.authentication.JWTAuthentication',
-  'rest_framework.authentication.TokenAuthentication',
  ],
  'DEFAULT_PARSER_CLASSES': [
   'rest_framework.parsers.JSONParser',
   'rest_framework.parsers.MultiPartParser',
- ],
- 'DEFAULT_PERMISSION_CLASSES': [
-  'rest_framework.permissions.IsAuthenticated',
  ],
 }
 
